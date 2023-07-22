c631d0d98fc827ea80d031bcd4f8cb021506cdf2
Michael Stang <mikestang808@gmail.com>
2023-07-21 19:16:00 -0500

    update

--- GPT/web-bot/bot.js
+++ GPT/web-bot/bot.js
@@ -5,7 +5,7 @@ const chatbotToggler = document.querySelector(".chatbot-toggler");
 const chatbotCloseBtn = document.querySelector(".close-btn");
 
 let userMessage;
-const API_KEY = "Your API Key";
+const API_KEY = "OPENAI_API_KEY";
 
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
